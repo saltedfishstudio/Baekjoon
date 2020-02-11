76fb46f13fa5fac941d6970f034a2b567ed26e8c
saltedfishstudio <saltedfishstudio@pm.me>
2020-02-12 01:06:08 +0900

    C# - 2839
    https://www.acmicpc.net/problem/2839

---
+++ Baekjoon/src/n2839.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baekjoon.src
+{
+    class n2839 : ISolutionBase
+    {
+        public string[] TestCase
+        {
+            get
+            {
+                return new string[] {
+                    "18",
+                "4",
+                "6",
+                "9",
+                "11",
+            };
+            }
+        }
+
+        public void Execute(string read)
+        {
+            int goal = int.Parse(read);
+            int start = goal / 5;
+
+            for(int i = start; i >= 0; i--)
+            {
+                int rest = goal - (i * 5);
+                if(rest % 3 == 0)
+                {
+                    Console.WriteLine(i + rest / 3);
+                    return;
+                }
+            }
+
+            Console.WriteLine(-1);
+        }
+    }
+}
