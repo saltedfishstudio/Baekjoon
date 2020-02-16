fceecfcd54b2235c3a81a6c49a0d4a12334d6eca
saltedfishstudio <saltedfishstudio@pm.me>
2020-02-16 15:26:06 +0900

    add n3046, n2163

--- Py-Baekjoon/src/solution.py
+++ Py-Baekjoon/src/solution.py
@@ -3,3 +3,15 @@ class Solution(object):
 	def n10699(self):
 		from datetime import date
 		print(date.today().strftime("%Y-%m-%d"))
+
+
+	def n3046(self):
+		x = input().split(' ')
+		print(int(x[0])*2-int(x[0]))
+
+	
+	def n2163(self):
+		x = input().split(' ')
+		n = int(x[0])
+		m = int(x[1])
+		print((int(n)) * (int(m)) -1)
