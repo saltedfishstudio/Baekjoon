9c1115384f3a153cd0f34552b8a2616aa2735645
saltedfishstudio <saltedfishstudio@pm.me>
2020-02-16 15:19:50 +0900

    py module
    a

---
+++ Py-Baekjoon/Py_Baekjoon.py
@@ -0,0 +1,6 @@
+
+import src
+
+if __name__ == "__main__":
+	sol = src.Solution()
+	sol.n10699()
---
+++ Py-Baekjoon/src/solution.py
@@ -0,0 +1,5 @@
+class Solution(object):
+	
+	def n10699(self):
+		from datetime import date
+		print(date.today().strftime("%Y-%m-%d"))
